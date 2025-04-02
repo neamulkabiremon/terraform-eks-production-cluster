68df17972aef06e7b56eb75bd94027ea5a7b237a
Neamul Kabir Emon <neamulkabiremon@gmail.com>
2025-04-02 11:31:57 +0600

    feat(day1): bootstrap EKS cluster with Terraform (VPC, Subnets, NAT, IGW, EKS, Node Group)

--- 8-nodes.tf
+++ 8-nodes.tf
@@ -58,7 +58,17 @@ resource "aws_eks_node_group" "general" {
   }
 
   labels = {
-    "kubernetes.io/cluster/${local.env}-${local.eks_name}" = "owned"
-    "kubernetes.io/role"                                  = "general"
+    role = "general"
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.amazon_eks_worker_node_policy,
+    aws_iam_role_policy_attachment.amazon_eks_cni_policy,
+    aws_iam_role_policy_attachment.amazon_ec2_container_registry_read_only,
+  ]
+
+  # Allow external changes without Terraform plan difference
+  lifecycle {
+    ignore_changes = [scaling_config[0].desired_size]
   }
 }
